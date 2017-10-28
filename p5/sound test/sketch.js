2419f45b08bc4379ce09435a7e3317971a1aa7d9
Jeff <qiuey00@gmail.com>
2017-10-28 16:00:30 -0400

    sound excercise 1

--- p5/sound test/sketch.js
+++ p5/sound test/sketch.js
@@ -1,7 +1,37 @@
-var sound;
+var mode;
+var mySound;
+var loopMode
 function preload(){
-	sound = loadSound('assets/song.mp3');
+	mySound = loadSound('assets/buzz.mp3');
 }
 function setup(){
-	mySound.play();
+	createCanvas(250,60);
+	loopMode = "restart"
+	
+}
+
+function draw(){
+	background(255);
+	mySound.playMode(loopMode);
+	textSize(32);
+	if (loopMode == "restart"){
+			text("Restart Mode",10,height);
+		}
+		else {
+			text("Sustain Mode",10,height);
+		}
+}
+
+function keyPressed(){
+	if (keyCode == ENTER){
+		if (loopMode == "restart"){
+			loopMode = "sustain";
+		}
+		else {
+			loopMode = "restart";
+		}
+	}
+}
+function mouseClicked() {
+  mySound.play();
 }
