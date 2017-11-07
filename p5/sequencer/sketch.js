005c0aafc63e5c809110e7cc611520406dc41334
Jeff <qiuey00@gmail.com>
2017-11-07 00:42:24 -0500

    bar color change

--- p5/sequencer/sketch.js
+++ p5/sequencer/sketch.js
@@ -14,6 +14,13 @@ function setup() {
 
 function draw() {
 	background(255);
+	
+	
+	for (var i = 0; i < notes.length; i++){
+		notes[i].isHit(bar.x);
+		notes[i].draw();
+	}
+	bar.draw();
 	fill(0);
 	textSize(40);
 	text(radi, 0,40);
@@ -21,11 +28,7 @@ function draw() {
 	if (millis() < 10000){
 		text("Press Enter to Reset; Left and Right to change circle radii; Up and Down to change tempo",70,40);
 	}
-	for (var i = 0; i < notes.length; i++){
-		notes[i].isHit(bar.x);
-		notes[i].draw();
-	}
-	bar.draw();
+
 	if (keyIsDown(UP_ARROW)){
 		bar.speedUP();
 	}
@@ -80,15 +83,6 @@ function mouseClicked() {
 }
 
 function keyPressed(){
-	// if(keyCode == LEFT_ARROW){
-	// 	radi-=1;
-	// 	if (radi == 0){
-	// 		radi = 0;
-	// 	}
-	// }
-	// if(keyCode == RIGHT_ARROW){
-	// 	radi+=1;
-	// }
 	if(keyCode == ENTER){
 		notes = [];
 	}
@@ -99,12 +93,15 @@ function Bar(){
 	this.speed = 1
 	this.draw = function(){
 		noStroke();
-		fill(0);
+		// fill(0);
 		rect(this.x,0,5,height);
 		this.x += this.speed;
 		if (this.x > width){
 			this.x = 0;
 		}
+	}
+	this.changeColor = function(aColor){
+		fill(aColor);
 	}
 	this.speedUP = function(){
 		this.speed += .3
@@ -128,6 +125,7 @@ function Note(aX,aY,radii, color){
 	this.sound = new p5.Oscillator(this.soundMap);
 	this.sound.start();
 	this.sound.amp(0);
+	this.color = color;
 
 	this.draw = function(){
 		if(this.on){
@@ -135,7 +133,7 @@ function Note(aX,aY,radii, color){
 		}
 	}
 	this.isClicked = function(){
-		if (this.on){	
+		if (this.on){
 			if (dist(mouseX,mouseY,this.x,this.y) < this.radi/2){
 				this.on = false;
 			}
@@ -144,11 +142,13 @@ function Note(aX,aY,radii, color){
 	this.isHit = function(barX){
 		if(barX+5 > this.x-this.radi/2 && barX < this.x+this.radi/2){
 			this.sound.amp(0.5,.05);
-			fill(color);
+			fill(this.color);
+			bar.changeColor(this.color);
 		}
 		else {
 			this.sound.amp(0,.05);
-			fill(0);	
+			// var a = color(0,0,0)
+			bar.changeColor("black");
 		}
 	}
 }
