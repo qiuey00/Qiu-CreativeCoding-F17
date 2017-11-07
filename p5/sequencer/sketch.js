1830b67474ccb20662db19f2a6f6b74eeadf4c8b
Jeff <qiuey00@gmail.com>
2017-11-07 00:24:38 -0500

    colors added

--- p5/sequencer/sketch.js
+++ p5/sequencer/sketch.js
@@ -18,7 +18,9 @@ function draw() {
 	textSize(40);
 	text(radi, 0,40);
 	textSize(20);
-	text("Press Enter to Reset",70,40);
+	if (millis() < 10000){
+		text("Press Enter to Reset; Left and Right to change circle radii; Up and Down to change tempo",70,40);
+	}
 	for (var i = 0; i < notes.length; i++){
 		notes[i].isHit(bar.x);
 		notes[i].draw();
@@ -42,8 +44,39 @@ function draw() {
 }
 
 function mouseClicked() {
-  var temp = new Note(mouseX,mouseY,radi);
-  append(notes,temp);
+	var notColor;
+	if(mouseX <= width/5){
+		var aMap = map(mouseX, 0,width/5,0,1);
+		var red = color(255,0,0);
+		var orange = color(255,127,0)
+		notColor = lerpColor(red,orange,aMap);
+	}
+	if((width/5) < mouseX && mouseX <= (width/5)*2){
+		var aMap = map(mouseX, width/5,(width/5)*2,0,1);
+		var yellow = color(255,255,0);
+		var orange = color(255,127,0);
+		notColor = lerpColor(orange,yellow,aMap);
+	}
+	if((width/5)*2 < mouseX && mouseX <= (width/5)*3){
+		var aMap = map(mouseX, (width/5)*2,(width/5)*3,0,1);
+		var yellow = color(255,255,0);
+		var green = color(0,255,0);
+		notColor = lerpColor(yellow,green,aMap);
+	}
+	if((width/5)*3 < mouseX && mouseX <= (width/5)*4){
+		var aMap = map(mouseX, (width/5)*3,(width/5)*4,0,1);
+		var blue = color(0,0,255);
+		var green = color(0,255,0);
+		notColor = lerpColor(green,blue,aMap);
+	}
+	if((width/5)*4 < mouseX && mouseX <= width){
+		var aMap = map(mouseX, (width/5)*4,width,0,1);
+		var blue = color(0,0,255);
+		var purple = color(75,0,130);
+		notColor = lerpColor(blue,purple,aMap);
+	}
+	var temp = new Note(mouseX,mouseY,radi, notColor);
+	append(notes,temp);
 }
 
 function keyPressed(){
@@ -66,7 +99,7 @@ function Bar(){
 	this.speed = 1
 	this.draw = function(){
 		noStroke();
-		fill(200,0,0);
+		fill(0);
 		rect(this.x,0,5,height);
 		this.x += this.speed;
 		if (this.x > width){
@@ -85,21 +118,21 @@ function Bar(){
 
 }
 
-function Note(aX,aY,radii){
+function Note(aX,aY,radii, color){
 	this.x = aX;
 	this.y = aY;
 	this.radi = radii;
 	this.on = true;
 	this.soundMap = map(this.y,0,height,1000,65);
-	this.colorMap = map(this.y,0,height,255,50);
+	// this.colorMap = map(this.y,0,height,255,50);
 	this.sound = new p5.Oscillator(this.soundMap);
 	this.sound.start();
 	this.sound.amp(0);
 
 	this.draw = function(){
 		if(this.on){
-		ellipse(this.x,this.y,this.radi, this.radi);
-	}
+			ellipse(this.x,this.y,this.radi, this.radi);
+		}
 	}
 	this.isClicked = function(){
 		if (this.on){	
@@ -111,7 +144,7 @@ function Note(aX,aY,radii){
 	this.isHit = function(barX){
 		if(barX+5 > this.x-this.radi/2 && barX < this.x+this.radi/2){
 			this.sound.amp(0.5,.05);
-			fill(0,0,this.colorMap);
+			fill(color);
 		}
 		else {
 			this.sound.amp(0,.05);
