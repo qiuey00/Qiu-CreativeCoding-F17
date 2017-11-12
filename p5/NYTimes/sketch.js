7605574778546f05b9b6d3c55bfdf0439aa088fa
Jeff <qiuey00@gmail.com>
2017-11-12 12:52:44 -0500

    boxes with inputs

--- p5/NYTimes/sketch.js
+++ p5/NYTimes/sketch.js
@@ -1,63 +1,52 @@
-// var myData;
-// function preload(){
-// 	myData = loadJSON("assets/heights.json")
-// }
-// function setup(){
-// 	createCanvvas(600,600);
-// 	println(myData.animals.length);
-// 	for(var i = 0; i < myData.animals.length; i++){
-// 		var thisElement = myData.animals[i].height;
-// 		consol.log(thisElement):
-// 	}
-// }
-// function draw(){
-
-// }
-
+//Partner: Abi
 var myData;
 var url;
 
 var searchTerms = [];
 var searchIndex = 0;
+var boxSearch = [];
+var queryInput;
 function setup(){
-	// baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
-	// query = "London,uk";
-	// appID = "APPID9097338f581bdc9ea7275ecb327b7a4d";
-	// url = baseUrl + "q=" + query + "&" + appID;
-	
-	url = "http://api.open-notify.org/iss-now.json";  //figure out how to access iss info
-
-	// searchTerms = ["cats","dogs","birds"]
-	// baseUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-	// appID = ;//need to register for an appID
-	// query = 
-	console.log(url)
-	myData = loadJSON(url);
-	console.log(myData);
+	createCanvas(1200,600);
+	searchTerms = ["Brooklyn","Manhattan","Queens"];
+	for (var i = 0; i < searchTerms.length; i++){
+		var myMap = map(i, 0, searchTerms.length, 100, width);
+		var aBox = new SearchBox(myMap-50,searchTerms[i],width/searchTerms.length);
+		append(boxSearch, aBox);
+	}
 }
 function draw(){
-	if(myData){
-		var posData = myData;  //figure out how to access iss info
-		// var keys = Object.keys(posData);
-		var dat = myData['iss_position'];
-		console.log(posData);
-		// console.log(keys);
+	for(var i = 0; i < boxSearch.length; i++){
+		boxSearch[i].draw();
 	}
-	else {
-		console.log("bad data!")
-	}
-}
-
-function dataResponse(tempData){
-	myData = tempData;
-}
-
-function queryData(){
-	loadJSON(url,dataResponse);
+	console.log(queryInput);
 }
 
 function mouseClicked(){
-	query = searchTerms[serchIndex]
-	searchIndex++;
-	url = baseUrl + "q=" + query + "&" + appID;
+	for(var i = 0; i < boxSearch.length; i++){
+		if(boxSearch[i].clicked()){
+			queryInput = boxSearch[i].seachTerm;
+		}
+	}
 }
+
+function SearchBox(aX,term, aLength){
+	this.x = aX;
+	this.y = 50;
+	this.length = aLength-100;
+	this.seachTerm = term;
+	this.draw = function(){
+		noFill();
+		rect(this.x, this.y, this.length, 100);
+		textSize(50);
+		fill(0);
+		text(this.seachTerm,this.x+40, this.y+70);
+	}
+	this.clicked = function(){
+		if(mouseX < this.x+this.length && mouseX > this.x && mouseY < this.y+100 && mouseY > this.y){
+			return true;
+		} else{
+			return false;
+		}
+	}
+};
